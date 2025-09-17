7581a9abc442ec1ff195eacddec66b09c28dc479
RobertoCrz <robertocarlosnunezcruz9@gmail.com>
2025-09-17 10:48:14 -0600

    detalle que cambie en .env

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -70,7 +70,7 @@ const useAuth = () => {
 
   const login = async (email: string, password: string): Promise<LoginResult> => {
     try {
-      const response = await fetch(`${API_URL}/login/`, {
+      const response = await fetch(`${API_URL}/auth/login/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),

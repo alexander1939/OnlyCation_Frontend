10cbf632f0666d5678ac62878176d88daf9254b3
Alex Cifuentes <alexcifuentes72818@gamil.com>
2025-10-04 22:48:30 -0600

    feat(certificado ssl)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
+    host: true,
     port: 5173,
-    allowedHosts: ["onlycation.local"], 
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'ssl/onlycation.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'ssl/onlycation.crt')),
+    },
+    allowedHosts: ["onlycation.local"],
   },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     port: 5173,
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, 'ssl/onlycation.key')),
+//       cert: fs.readFileSync(path.resolve(__dirname, 'ssl/onlycation.crt')),
+//     },
+//     allowedHosts: ["onlycation.local"],
+//   },
+// })
+

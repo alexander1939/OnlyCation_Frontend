4e3a13f8235d4382c98d831d9a299c19b3116819
Alex Cifuentes <alexcifuentes72818@gamil.com>
2025-11-09 17:28:27 -0600

    feat: Conexion a la base de datos del servidor

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0fcee9ce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hd8enqmnqlo"
+    "revision": "0.k5f9u4sn4m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

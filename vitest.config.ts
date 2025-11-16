977b1c4a71e1426050630279cf58606516735d83
Alex Cifuentes <alexcifuentes72818@gamil.com>
2025-11-16 17:23:56 -0600

    fix: correcion de .yml

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     ],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html', 'lcov'],
+      reporter: ['text', 'json', 'lcov'],
       exclude: [
         'node_modules/',
         'src/test/',

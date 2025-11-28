eae721b93c9e9b2ef33fc456e72a6a0092df256e
RobertoCrz <robertocarlosnunezcruz9@gmail.com>
2025-11-27 23:23:56 -0600

    fix: correcion de la ruta de activar cuenta

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0fcee9ce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ldj23imb5f"
+    "revision": "0.9k5ilr4kdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

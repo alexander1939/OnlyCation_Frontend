af31634f0c289558d4e6dc26663e0e5510fa14c0
Jesus Guzman Jimenez <145624215+MILAHUU@users.noreply.github.com>
2025-11-29 13:57:45 -0600

    feat: implementa actualización de video de presentación, como tambien de datos personales

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-041f2bd8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.bkadtd4nmro"
+    "revision": "0.67u7bev0ea"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {

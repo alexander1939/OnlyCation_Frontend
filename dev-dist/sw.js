fc1b7eb1e9927db6aad57f5b46576d23c9ae3238
Jesus Guzman Jimenez <145624215+MILAHUU@users.noreply.github.com>
2025-11-29 16:50:19 -0600

    feat: add comprehensive validation for profile name fields

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-041f2bd8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.96tm6ekpe9k"
+    "revision": "0.h8duba83vqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
